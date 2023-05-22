8d6e5623688c97425acbb126bfcfbaea860a1881
Xylitol311 <cktnghd0311@daum.net>
2023-05-22 21:27:11 +0900

    계좌 생성 API 만들기 위한 도메인, 컨트롤러, dto, config 설정(1)

--- src/main/java/com/example/account/domain/AccountUser.java
+++ src/main/java/com/example/account/domain/AccountUser.java
@@ -5,31 +5,25 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Account {
+public class AccountUser {
     @Id
     @GeneratedValue
     private Long id;
 
-    @ManyToOne
-    private AccountUser accountUser;// DB쪽 user 테이블과 충돌 방지를 위해 다른 이름 설정
-    private String accountNumber;
-
-    @Enumerated(EnumType.STRING)
-    private AccountStatus accountStatus;
-    private Long balance;
-
-    private LocalDateTime registeredAt;
-    private LocalDateTime unRegisteredAt;
+    private String name;
 
     @CreatedDate
     private LocalDateTime createdAt;
